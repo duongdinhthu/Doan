6a08b37ebb72372e321945e7bc2b271a5b5b7f4b
Dương Thứ <thuddth2307004@fpt.edu.vn>
2024-02-22 09:08:27 +0700

    up

--- backend/Docanhan.php
+++ backend/Docanhan.php
@@ -1,59 +1,145 @@
+<?php
+session_start();
+//kiểm tra session
+if (!isset($_SESSION["username"])&&!isset($_SESSION['password'])){
+    header("Location:http://localhost:63342/DoanKI1/frontend/home.html");
+    exit;
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <title>Bootstrap Image Slider</title>
+    <title>Title</title>
 </head>
 <body>
+<head>
+    <meta charset="UTF-8">
+    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400..700&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="home.css">
+    <link rel="stylesheet" href="Docanhan.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous"> <link rel="stylesheet" href="trangchu.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>Home</title>
 
-<div id="mainCarousel" class="carousel slide" data-ride="carousel">
-    <div class="carousel-inner">
-        <div class="carousel-item active">
-            <img src="https://bizweb.dktcdn.net/100/440/011/products/sp5-5.jpg?v=1634875263327" class="d-block w-100" alt="Main Image">
+</head>
+<body>
+<header>
+    <div class="header">
+        <div class="header1">
+            <img src="img/logo.webp" alt="logo">
+            <label>
+                <input type="text" placeholder="Search">
+                <i class="fa-solid fa-magnifying-glass"></i>
+            </label>
+            <div class="hotline">
+                <i class="fa-solid fa-phone"></i>
+
+                <p>Hotline<br>78910JQKA</p>
+            </div>
         </div>
-        <!-- Thêm các slide khác nếu cần -->
-    </div>
-</div>
+        <div class="d-flex">
+            <a href="home.php" class="btn btn-outline">Home</a>
+            <div class="dropdown">
+                <button class="dropbtn">Games</button>
+                <div class="dropdown-content">
+                    <a href="Trochoitreem.php">Kids</a>
+                    <a href="Trochoigiadinh.php">Family</a>
+                    <a href="Trochoinguoilon.php">Adults</a>
+                </div>
+            </div>
+            <div class="dropdown">
+                <button class="dropbtn">Products</button>
+                <div class="dropdown-content">
+                    <a href="Docanhan.php">Personal items</a>
+                    <a href="Dothietyeu.php">Essentials</a>
+                    <a href="Thucpham.php">Food</a>
+                </div>
+            </div>
+            <div class="dropdown">
+                <button class="dropbtn">Services</button>
+                <div class="dropdown-content">
+                    <a href="Dichvutrongoi.php">Packages</a>
+                    <a href="Dichvutour.php"> Tours</a>
+                    <a href="Dichvutochuc.php">Organizing</a>
+                </div>
+            </div>
+            <a href="tintuc.php" class="btn btn-outline">News</a>
+            <a href="lienhe.php" class="btn btn-outline">Contact</a>
+            <a href="user.php" class="btn btn-outline">Account</a>
 
-<div id="thumbnailCarousel" class="carousel slide" data-ride="carousel">
-    <div class="carousel-inner">
-        <div class="carousel-item active">
-            <div class="d-flex">
-                <div class="thumbnail" data-target="#mainCarousel" data-slide-to="0">
-                    <img src="https://bizweb.dktcdn.net/100/440/011/products/sp5.jpg?v=1634875258950" class="d-block w-100" alt="Thumbnail 1">
+        </div>
+    </div>
+    <img src="https://bizweb.dktcdn.net/100/440/011/themes/894889/assets/img_banner_brea_col.jpg?1702953098418" alt="" style="width:100%;">
+</header>
+
+
+<div class="footer">
+    <div class="footer0">
+        <div class="footer1">
+            <img src="img/logo.webp" alt="">
+            <div class="icon">
+                <i class="fa-brands fa-instagram"></i>
+                <i class="fa-brands fa-facebook"></i>
+                <i class="fa-brands fa-youtube"></i>
+                <i class="fa-brands fa-twitter"></i>
+            </div>
+            <p>SUBSCRIBE TO NEWSLETTER</p>
+            <label>
+                <input type="email" placeholder="Enter email address">
+                <button>SEND</button>
+            </label>
+            <div class="contact">
+                <i class="fa-solid fa-location-dot"></i>
+                <p>.................................</p>
+            </div>
+            <div class="contact">
+                <i class="fa-solid fa-phone"></i>
+                <p>0910JQKA</p>
+            </div>
+            <div class="contact">
+                <i class="fa-solid fa-headphones"></i>
+                <p>support@gmail.com</p>
+            </div>
+        </div>
+        <div class="footer2">
+            <div class="paysupport">
+                <div class="Support">
+                    <h3>ABOUT US</h3><br>
+                    <a href="#">Company</a><br>
+                    <a href="#">Address</a><br>
+                    <a href="#">Phone number</a><br>
+                    <a href="#">Links</a><br>
                 </div>
-                <div class="thumbnail" data-target="#mainCarousel" data-slide-to="1">
-                    <img src="https://bizweb.dktcdn.net/100/440/011/products/sp5-2.jpg?v=1634875260253" class="d-block w-100" alt="Thumbnail 2">
+                <div class="Support">
+                    <h3>CUSTOMER SUPPORT</h3><br>
+                    <a href="#">Contact</a><br>
+                    <a href="#">Handling complaints</a><br>
+                    <a href="#">Usage guide</a><br>
+                    <a href="#">Warranty, returns</a><br>
+                    <a href="#">Contact</a><br>
                 </div>
-                <div class="thumbnail" data-target="#mainCarousel" data-slide-to="2">
-                    <img src="https://bizweb.dktcdn.net/100/440/011/products/sp5-3.jpg?v=1634875261313" class="d-block w-100" alt="Thumbnail 3">
+                <div class="Support">
+                    <h3>SERVICES</h3><br>
+                    <a href="#">Products</a><br>
+                    <a href="#">Game organization services</a><br>
+                    <a href="#">Payment</a><br>
                 </div>
-                <div class="thumbnail" data-target="#mainCarousel" data-slide-to="3">
-                    <img src="https://bizweb.dktcdn.net/100/440/011/products/sp5-4.jpg?v=1634875262273" class="d-block w-100" alt="Thumbnail 4">
-                </div>
-                <div class="thumbnail" data-target="#mainCarousel" data-slide-to="4">
-                    <img src="https://bizweb.dktcdn.net/100/440/011/products/sp5-5.jpg?v=1634875263327" class="d-block w-100" alt="Thumbnail 5">
-                </div>
+            </div>
+            <div class="pay">
+                <h3>PAYMENT METHODS</h3>
+                <button></button>
+                <button></button>
+                <button></button>
+                <button></button>
             </div>
         </div>
     </div>
+    <hr/>
+    <p class="license">@ Copyright belongs to ... | Provided by ...</p>
 </div>
-
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.0.7/dist/umd/popper.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-
-<script>
-    // Xử lý sự kiện khi click vào hình ảnh phụ
-    $(document).ready(function(){
-        $('.thumbnail').click(function(){
-            var targetSlide = $(this).attr('data-slide-to');
-            $('#mainCarousel').carousel(parseInt(targetSlide));
-        });
-    });
-</script>
-
+<script src="trangchu.js"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
 </body>
 </html>
